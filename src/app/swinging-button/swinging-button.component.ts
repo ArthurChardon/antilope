916daf67210f824b13a2449f337d8924b4559ed6
ArthurChardon <arthur.chardon74@gmail.com>
2022-09-28 17:11:50 +0200

    added randomness for swing on swinging buttons

--- src/app/swinging-button/swinging-button.component.ts
+++ src/app/swinging-button/swinging-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser'
 import { faBook, faSkull, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -7,6 +8,11 @@ import { faBook, faSkull, faHouse } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./swinging-button.component.scss']
 })
 export class SwingingButtonComponent implements OnInit {
+  @HostBinding("attr.style")
+  public get valueAsStyle(): any {
+    return this.sanitizer.bypassSecurityTrustStyle(`--animation-time: ${this.duration}ms; --angle-max: ${this.angle}`);
+  }
+
   @Input() icon = 'house';
   @Input() color = 'black';
   @Input() length: 'large' | 'medium' | 'small' | 'random' = 'medium';
@@ -15,7 +21,10 @@ export class SwingingButtonComponent implements OnInit {
   faBookIcon = faBook; faSkullIcon = faSkull; faHouseIcon = faHouse;
   faIcon = faHouse;
 
-  constructor() { }
+  duration = 2000;
+  angle = 20;
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     switch(this.icon) {
@@ -33,6 +42,21 @@ export class SwingingButtonComponent implements OnInit {
         this.faIcon = faHouse;
         break;
     }
+
+    this.duration = this.getRandomIntFromInterval(1200, 2200);
+    this.angle = this.getRandomIntFromInterval(15, 25);
+  }
+
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  getRandomIntFromInterval(min: number, max: number) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  getRandom(max: number) {
+    return Math.random() * max;
   }
 
 }
